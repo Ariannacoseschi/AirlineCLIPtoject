f5d635626aa6883b2b3dcbc33e0311eebfbe6b6c
Rashad Ramali <rashad.ramali@hotmail.co.uk>
2023-10-24 13:40:33 +0100

    Added Passenger testing

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PassengerTest {
 
@@ -12,4 +15,37 @@ public class PassengerTest {
    }
 
 
+
+    @Test
+    void canGetId() {
+       assertThat(passenger.getId()).isEqualTo(1);
+    }
+
+    @Test
+    void canSetId() {
+       passenger.setId(2);
+       assertThat(passenger.getId()).isEqualTo(2);
+    }
+
+    @Test
+    void getName() {
+       assertThat(passenger.getName()).isEqualTo("Colin");
+    }
+
+    @Test
+    void setName() {
+       passenger.setName("Zsolt");
+       assertThat(passenger.getName()).isEqualTo("Zsolt");
+    }
+
+    @Test
+    void getContactInfo() {
+       assertThat(passenger.getContactInfo()).isEqualTo("dragonturtle@gmail.com");
+    }
+
+    @Test
+    void setContactInfo() {
+       passenger.setContactInfo("dinoturtle@gmail.com");
+       assertThat(passenger.getContactInfo()).isEqualTo("dinoturtle@gmail.com");
+    }
 }
